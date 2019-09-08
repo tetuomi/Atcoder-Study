e525de17908572eacb68f130c74d6644f005eb93
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-09-08 22:59:18 +0900

    Answered correctly A , B and C problems

---
+++ Contest-140/Contest140_A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+  int n;
+  cin >> n;
+
+  cout << n * n * n;
+  
+  return 0;    
+}
+
---
+++ Contest-140/Contest140_D.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+  int n,k;
+  cin >> n >> k;
+  string s;
+  cin >> s;
+
+  
+  
+  return 0;    
+}
+
