4acdd8b6a502f4da7d3d904c579280abf34ea315
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-12-26 17:29:38 +0900

    Add code

---
+++ Study/Educational-DP-Contest/F.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+  string s,t;
+  cin >> s >> t;
+
+  int sl = s.size();
+  int tl = t.size();
+  
+  vector<vector<string>> dp(3001,vector<string>(3001));
+  
+  
+
+  
+  return 0;
+}
+
